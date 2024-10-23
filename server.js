5e940e106f6a773b06dd3c905a580106a9434409
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-10-23 20:47:01 +0900

    docs: ejs 셋팅 1. [npm install ejs] 2. server.js에 app.set 1줄 추가

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+
+app.set('view engine', 'ejs')
+
 app.listen(8080, () => {
     console.log('listening on 8080: http://localhost:8080')
 })
