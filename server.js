b7f8f4f6155f5b1a5f5c9d24f2e513ecc9f024cd
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-10-23 21:58:34 +0900

    fix: 아 href 에 오타 있었음. kr-KR

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const i18n = require('i18n');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+
 
 i18n.configure({
     locales: ['en-US', 'ko-KR'],
@@ -11,6 +14,7 @@ i18n.configure({
     queryParameter: 'lang'
   });
   
+app.use(cookieParser());
 app.use(i18n.init); // i18n 미들웨어 추가
 
 app.set('view engine', 'ejs')
@@ -19,7 +23,9 @@ app.listen(8080, () => {
     console.log('listening on 8080: http://localhost:8080')
 })
 
+
 app.get('/', (request, response) => {
+    console.log("현재 언어 설정: ", request.getLocale());
     response.render('index.ejs')
 })
 
@@ -30,6 +36,6 @@ app.get('/login', (request, response) => {
 app.get('/change-lang/:lang', (request, response) => { //언어 설정 변경
     console.log(request.params.lang);
     const lang = request.params.lang;
-    response.cookie('lang', lang, {maxAge: 1000000, httpOnly: true});
-    response.redirect(request.get("Referrer") || "/");
+    response.cookie('lang', lang, {maxAge: 1000 * 60 * 60 * 24 * 7, httpOnly: false}); //밀리초 단위 > 7일 쿠키
+    response.redirect("/");
 })
