bbe240ce0dedc1b5465c24cd1efed5c6c7cc7b14
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-10-23 21:53:21 +0900

    docs: [npm install cookie-parser]

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.get('/change-lang/:lang', (request, response) => { //언어 설정 변경
     console.log(request.params.lang);
     const lang = request.params.lang;
     response.cookie('lang', lang, {maxAge: 1000000, httpOnly: true});
-    response.location(request.get("Referrer") || "/");
+    response.redirect(request.get("Referrer") || "/");
 })
