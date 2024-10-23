b5265014d6bbc980c4ad646a1a9e444388cf999c
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-10-24 02:09:44 +0900

    fix: post로 보낸 request 안 나오던 문제 해결. app.use(express.json()) app.use(express.urlencoded({extended:true}))

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+//request.body 미리 풀어줘요.
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) 
+
 const dbUri = process.env.DB_URI;
 
 i18n.configure({
@@ -15,78 +19,118 @@ i18n.configure({
     defaultLocale: 'en-US',
     cookie: 'lang',
     queryParameter: 'lang'
-  });
+  })
   
 app.use(cookieParser()); //왜 그런진 모르겠지만 init 아래로 내리니까 안되더라 조심!
 app.use(i18n.init); // i18n 미들웨어 추가
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 //MongoDB 셋팅
-const { MongoClient } = require('mongodb')
-let db
+const { MongoClient } = require('mongodb');
+let db;
 const url = dbUri;
 new MongoClient(url).connect().then((client)=> {
-    console.log('DB connected')
-    db = client.db('trieeDB')
+    console.log('DB connected');
+    db = client.db('trieeDB');
 
     app.listen(8080, () => {
-        console.log('listening on 8080: http://localhost:8080')
+        console.log('listening on 8080: http://localhost:8080');
     })
     
 }).catch((err)=> {
-    console.log(err)
+    console.log(err);
 })
 
 //--------------------------------------------------------------
 //session passport login 가져오기 시작
-const session = require('express-session')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 app.use(session({
   secret: '암호화에 쓸 비번', //털리면 큰일나요!
   resave : false,
-  saveUninitialized : false
+  saveUninitialized : false,
+  cookie : { maxAge : 1000 * 60 * 60 } //session cookie 1시간 유지
 }))
-app.use(passport.session()) 
+app.use(passport.session());
 
 
 passport.use(new LocalStrategy(async (usernameInput, passwordInput, cb) => {
-    let result = await db.collection('users').findOne({ username : usernameInput})
+    let result = await db.collection('users').findOne({ username : usernameInput});
     if (!result) {
-      return cb(null, false, { message: i18n.t('errors_usernameNotFound') })
+      return cb(null, false, { message: 'invalid username' }); //i18n.t(errors_usernameNotFound)
     }
     if (result.password == passwordInput) {
-      return cb(null, result)
+      return cb(null, result);
     } else {
-      return cb(null, false, { message: i18n.t('errors_passwordMismatch') });
+      return cb(null, false, { message: 'invalid password' }); //i18n.t(errors_passwordMismatch)
     }
   }))
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  process.nextTick(() => { //node.js에서 내부 코드 비동기적으로 처리 - 너무 길어지면 괄호 안에 있는 것 다음에
+    done(null, { id: user._id, username: user.username });
+  })
+})
+
+passport.deserializeUser( async(user, done) => { //cookie 분석 > 어디서든 request.user 사용 가능하게 해줍니다.
+  let result = await db.collection('user').findOne({_id: new ObjectId(user.id)});
+  delete result.password; //비번은 삭제
+  process.nextTick(() => { 
+    done(null, result);
+  })
+})
 //session passport login 가져오기 끝
 //--------------------------------------------------------------
 
 app.get('/', (request, response) => {
-    console.log("현재 언어 설정: ", request.getLocale());
-    response.render('index.ejs')
+    console.log("current language setting: ", request.getLocale());
+    response.render('index.ejs');
+})
+
+app.get('/signup', (request, response) => {
+  response.render('signup.ejs');
+})
+
+app.post('/signup', (request, response) => {
+  //서버에 계정 정보 추가하기
+  console.log(request.body);
+
 })
 
 app.get('/login', (request, response) => {
-    response.render('login.ejs')
+    console.log(request.user);
+    response.render('login.ejs');
 })
 
-app.post('/login', async (request, response, next)=>{
-    passport.authenticate('local', (error, user, info) => {
-      if (error) return response.status(500).json(error);
-      if (!user) return response.status(401).json(info.message); //info.message는 상단에 선언
-      request.logIn(user, (err) => { //session 생성
-        if (err) return next(err);
-        response.redirect('/') // 로그인 완료 시 리다이렉트
-      }) 
+app.post('/login', async (request, response, next) => {
+  console.log('login button pressed');
 
-    })(request, response, next)
+  passport.authenticate('local', (error, user, info) => {
+    console.log('passport auth start');
+    // if (error) {
+    //   console.log('server error');
+    //   return response.status(500).json(error);
+    // }
+    // if (!user) {
+    //   console.log('invalid username or password')
+    //   return response.status(401).json(info.message);
+    // }
+    request.logIn(user, (err) => {
+      if (err) {
+        console.log('server error 2')
+        return next(err);
+      }
+      console.log('just before the page redirects')
+      response.redirect('/');
+    })
+  })(request, response, next)
 
 })
+
 
 app.get('/change-lang/:lang', (request, response) => { //언어 설정 변경
     console.log(request.params.lang);
